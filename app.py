edc133dfec62a1608a4e7b25a87f5b76497095a5
sanjaypoojary56-art <sanjaypoojary56@gmail.com>
2026-04-05 10:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,11 @@ def home():
             result1=ai_response(text)            
     return render_template("index.html",result=result1)
 def ai_response(text):
-    client=gimini.Client(api_key="AIzaSyAiPkeqWfQZEueGbC3m0nNLaQi7Mc4-64Q")
-    response=client.models.generate_content(model="gemini-2.5-flash",contents=f" First give greetings like hello i am here and then Analyze the resume and give improvements suggestions \n:{text}")
+    client=gimini.Client(api_key="AIzaSyCE7CrK6KkhQz_Xj3SFHaQqbbYblCD9KWE")
+    response=client.models.generate_content(model="gemini-2.5-flash",contents=f" First give greetings like hello i am here and then Analyze the resume and give improvements suggestions  and give short and sweet \n:{text}")
     return response.text
     
   
 if __name__=="__main__":
-    app.run(debug=True)
-
-
-
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
